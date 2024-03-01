862bf126b23e55306bcb501c8681a8ae4d748e10
DucTuan <tuanhypc02@gmail.com>
2024-03-01 21:55:36 +0700

    Initial Commit

---
+++ firstSwiftUI/firstSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  firstSwiftUIApp.swift
+//  firstSwiftUI
+//
+//  Created by macOS on 01/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct firstSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
