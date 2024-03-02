33a2665799914fe187f269d9e268e73e043a724e
DucTuan <tuanhypc02@gmail.com>
2024-03-02 18:18:08 +0700

    second View

---
+++ firstSwiftUI/secondView.swift
@@ -0,0 +1,20 @@
+//
+//  secondView.swift
+//  firstSwiftUI
+//
+//  Created by macOS on 02/03/2024.
+//
+
+import SwiftUI
+
+struct secondView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct secondView_Previews: PreviewProvider {
+    static var previews: some View {
+        secondView()
+    }
+}
