89ce4bab813d6af5d966c5bce5ad94f30d77dda6
DucTuan <tuanhypc02@gmail.com>
2024-03-03 16:31:53 +0700

    stack

--- firstSwiftUI/ContentView.swift
+++ firstSwiftUI/ContentView.swift
@@ -10,18 +10,13 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Test SwiftUI").font(SwiftUI.Font.headline)
-            
+            Image("avt")
+                .padding()
         }
-        .padding()
     }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
-}
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
+    }}
